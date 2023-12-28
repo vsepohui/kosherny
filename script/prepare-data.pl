75eeb41cfb6facbd7a0967a9a7bd8ec8c2e49ff8
Ivan Trunaev <trunaev.ivan@icloud.com>
2023-12-28 17:59:40 +0500

    new

---
+++ script/prepare-data.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use lib $Bin . '/../lib/';
+use 5.022;
+use Kosherny::Config;
+use JSON;
+
+
+1;
--- script/t-me-sucker.pl
+++ script/t-me-sucker.pl
@@ -6,6 +6,10 @@ use FindBin qw($Bin);
 use lib $Bin . '/../lib/';
 use 5.022;
 use Kosherny::Config;
+use JSON;
+use Date::Parse;
+use AnyEvent;
+use AnyEvent::HTTP;
 
 my $config = new Kosherny::Config;
 
@@ -26,13 +30,35 @@ sub parse_html {
 	return $page;
 }
 
+my $cv = AnyEvent::condvar;
+
 for my $url (@{$config->{t_me}}) {
 	my $url2 = $url;
 	$url2 =~ s/t\.me\//t.me\/s\//;
 	my $html = fetch($url2);
 	my $last_id = parse_html($url, $html);
-	warn $last_id;
+	for (1..$last_id) {
+		my $u = $url . '/' . $_;
+		
+		http_get $u . '?embed=1&mode=tme', sub {
+			warn $u;
+			$html = $_[1];
+			next if $html =~ /VIEW IN TELEGRAM/i;
+			#die $u;
+			$html =~ /\<div class=\"tgme_widget_message_text js-message_text\" dir=\"auto\"\>(.+?)\<\/div\>/;
+			my $text = $1;
+			my $image = '';
+			if ($html =~ /background-image:url\(\'([^\>]+)\'/) {
+				$image = $1;
+			}
+			$html =~ /\<time datetime=\"([^\"]+)\"/;
+			my $date = $1;
+			say encode_json([$u, str2time($date), $text, $image]);
+		}
+	}
 	
 }
+
+$cv->recv;
 
 1;
