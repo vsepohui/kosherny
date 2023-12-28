86d084d5611979b52825a7b6e3f4518eb6e289e0
Ivan Trunaev <trunaev.ivan@icloud.com>
2023-12-28 18:21:52 +0500

    fix

--- script/prepare-data.pl
+++ script/prepare-data.pl
@@ -7,6 +7,37 @@ use lib $Bin . '/../lib/';
 use 5.022;
 use Kosherny::Config;
 use JSON;
+use utf8;
+use Encode;
+
+sub _slurp_file {
+	my $file = shift;
+
+	my $s;
+	my $fi;
+	open $fi, '<:encoding(utf8)', $file or return;
+	$s = join '', <$fi>;
+	close $fi;
+	
+	return $s;
+}
+
+my $vk  = _slurp_file($Bin. '/vk.dump');
+my $tme = _slurp_file($Bin. '/t-me.dump');
+
+my @stream; 
+
+for my $s (split /\n/, $vk . $tme) {
+	warn $s;
+	push @stream, decode_json  Encode::encode_utf8($s);
+}
+
+@stream = sort {$b->[1] <=> $a->[1]} @stream;
+
+for (@stream) {
+	say encode_json(\$_);
+}
+	
 
 
 1;
--- script/t-me-sucker.pl
+++ script/t-me-sucker.pl
@@ -5,11 +5,14 @@ use warnings;
 use FindBin qw($Bin);
 use lib $Bin . '/../lib/';
 use 5.022;
+use utf8;
 use Kosherny::Config;
-use JSON;
+use JSON::XS;
 use Date::Parse;
-use AnyEvent;
-use AnyEvent::HTTP;
+use Encode;
+
+
+use Data::Dumper;
 
 my $config = new Kosherny::Config;
 
@@ -30,8 +33,6 @@ sub parse_html {
 	return $page;
 }
 
-my $cv = AnyEvent::condvar;
-
 for my $url (@{$config->{t_me}}) {
 	my $url2 = $url;
 	$url2 =~ s/t\.me\//t.me\/s\//;
@@ -39,26 +40,25 @@ for my $url (@{$config->{t_me}}) {
 	my $last_id = parse_html($url, $html);
 	for (1..$last_id) {
 		my $u = $url . '/' . $_;
-		
-		http_get $u . '?embed=1&mode=tme', sub {
-			warn $u;
-			$html = $_[1];
-			next if $html =~ /VIEW IN TELEGRAM/i;
-			#die $u;
-			$html =~ /\<div class=\"tgme_widget_message_text js-message_text\" dir=\"auto\"\>(.+?)\<\/div\>/;
-			my $text = $1;
-			my $image = '';
-			if ($html =~ /background-image:url\(\'([^\>]+)\'/) {
-				$image = $1;
-			}
-			$html =~ /\<time datetime=\"([^\"]+)\"/;
-			my $date = $1;
-			say encode_json([$u, str2time($date), $text, $image]);
+		warn $u;
+		my $html = fetch($u . '?embed=1&mode=tme');
+
+		next if $html =~ /VIEW IN TELEGRAM/i;
+		#die $html;
+		#die $u;
+		$html =~ /\<div class=\"tgme_widget_message_text js-message_text\" dir=\"auto\"\>(.+?)\<\/div\>/;
+		my $text = $1;
+		my $image = '';
+		if ($html =~ /background-image:url\(\'([^\>]+)\'/) {
+			$image = $1;
 		}
+		$html =~ /\<time datetime=\"([^\"]+)\"/;
+		my $date = $1;
+		say JSON::XS->new->utf8->encode([$u, str2time($date), Encode::decode_utf8($text), $image]);
 	}
 	
 }
 
-$cv->recv;
+
 
 1;
