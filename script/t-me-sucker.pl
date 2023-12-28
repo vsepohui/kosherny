c7f390ad3e0e46a02d362d74f501b035d0368703
Ivan Trunaev <trunaev.ivan@icloud.com>
2023-12-28 17:27:19 +0500

    fix

--- script/t-me-sucker.pl
+++ script/t-me-sucker.pl
@@ -6,55 +6,33 @@ use FindBin qw($Bin);
 use lib $Bin . '/../lib/';
 use 5.022;
 use Kosherny::Config;
-use JSON;
 
 my $config = new Kosherny::Config;
-my $token = $config->{vk_token};
 
 sub fetch {
-	my %params = @_;
-	my $url = "https://api.vk.com/method/wall.get?".join '&', map {$_ . '=' . $params{$_}} sort keys %params;
-	my $json = `curl -s "$url"`;
-	return decode_json $json;
+	my $url = shift;
+	return `curl -s "$url"`;
 }
 
-for my $feed (@{$config->{vk_feeds}}) {
-	my $feed2 = $feed;
-	$feed =~ s/^https\:\/\/vk\.com\/(.+)$/$1/;
-	
-	my $offset = 0;
-	
-	my $d = fetch(
-		domain       => $feed,
-		count        => 100,
-		offset       => $offset,
-		access_token => $token,
-		v            => '5.81',
-	);
-	
-	for (@{$d->{response}->{items}}) {
-		say encode_json ([$feed2, $_->{date}, $_->{text}]);
+sub parse_html {
+	my $url  = shift;
+	my $html = shift;
+
+	my $page;
+	while ($html =~ m/href=\"$url\/(\d+)\"/gim) {
+		$page = $1;
 	}
 	
-	if ($d->{response}->{count} > 100) {
-		while ($offset + 100 < $d->{response}->{count}) {
-			$offset += 100;
-			my $d = fetch(
-				domain       => $feed,
-				count        => 100,
-				offset       => $offset,
-				access_token => $token,
-				v            => '5.81',
-			);
+	return $page;
+}
 
-
-			for (@{$d->{response}->{items}}) {
-				say encode_json ([$feed2, $_->{date}, $_->{text}]);
-			}
-			
-		}
-			
-	}
+for my $url (@{$config->{t_me}}) {
+	my $url2 = $url;
+	$url2 =~ s/t\.me\//t.me\/s\//;
+	my $html = fetch($url2);
+	my $last_id = parse_html($url, $html);
+	warn $last_id;
+	
 }
 
 1;
